1fab546d4fa241a43cbc81f21f4024b686d2e1fe
Simen Krantz Knudsen <simenkk@ntnu.no>
2020-09-16 09:45:19 +0200

    First commit

---
+++ project_ttk28.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader as dl
+from math import sqrt
+
+# Manual seed for repeatability
+ranSeed = 96554
+torch.manual_seed(ranSeed)
